9283329f650881c570d80ded8c44df41f8f796dd
shawnjing <sshawnn128@gmail.com>
2019-07-19 15:20:08 +0800

    空项目模板提交

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'// 多设备样式不统一问题
+import './assets/styles/border.css'// 解决不同设备分辨率 1像素边框问题
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
