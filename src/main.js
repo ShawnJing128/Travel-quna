e6486a854a95cf9d777f616b56dc3ed2a02ffe61
shawnjing <sshawnn128@gmail.com>
2019-07-21 16:49:35 +0800

    swiper-change

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import fastClick from 'fastclick'
 import 'styles/reset.css'// 多设备样式不统一问题
 import 'styles/border.css'// 解决不同设备分辨率 1像素边框问题
 import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
