b9ffe628b3d39e39a91ea78e13b7c11691c68975
shawnjing <sshawnn128@gmail.com>
2019-07-21 14:49:05 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'// 多设备样式不统一问题
-import './assets/styles/border.css'// 解决不同设备分辨率 1像素边框问题
-
+import 'styles/reset.css'// 多设备样式不统一问题
+import 'styles/border.css'// 解决不同设备分辨率 1像素边框问题
+import 'styles/iconfont.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 /* eslint-disable no-new */
